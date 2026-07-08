13eca2a478e11972299e20a92df7c3ff6ae8607f
netanel <netanelbenza@gmail.com>
2026-07-08 20:57:02 +0300

    Update Data Menjer file

---
+++ Data_Manager.js
@@ -0,0 +1,18 @@
+import fs from 'fs/promises'
+
+
+
+export async function readDataToJson(path) {
+    const lodingData  = await fs.readFile(path,'utf-8')
+    const createObj = JSON.parse(lodingData)
+    return createObj 
+    
+}
+
+
+
+export async function writeDataToJson(path,data) {
+    const WriteData  = await fs.writeFile(path,JSON.stringify(data,null,2),'utf-8')
+}
+
+
--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const PORT = process.env.PORT
 const server = express()
 
 
+server.get('/',(req,res) =>{
+    res.json({message: 'The server running now'})
+})
+
+
+server.get('/health',(req,res)=>{
+    res.json({massage:'The server started successfully'})
+})
+
+
 
 
 
